da739cd84afaf956fea0c704471f246d716e9620
imamfahr <imamfahr@gmail.com>
2020-09-03 22:17:19 +0700

    twitter copy cat half-done

--- twitterCopyCat/script-twitter.js
+++ twitterCopyCat/script-twitter.js
@@ -1,2 +1,12 @@
 let tweet = [];
 
+
+function myFunc (){
+    tweet.push(document.getElementById('tweetInput').value);
+    for (let i = 0; i< tweet.length;i++){
+        document.getElementById('timeLine').appendChild
+    }
+}
+
+
+console.log(tweet)
