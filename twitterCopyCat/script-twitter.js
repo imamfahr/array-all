926909497c26b564fc77e7f1d27099f29326c23e
imamfahr <imamfahr@gmail.com>
2020-09-10 10:55:29 +0700

    fix twitter copycat

--- twitterCopyCat/script-twitter.js
+++ twitterCopyCat/script-twitter.js
@@ -1,11 +1,18 @@
 let tweet = [];
+let tweetContainer = document.getElementById('timeLine');
 
 
 function myFunc (){
     tweet.push(document.getElementById('tweetInput').value);
-    for (let i = 0; i< tweet.length;i++){
-        document.getElementById('timeLine').appendChild
-    }
+    
+        // document.getElementById('timeLine').appendChild(tweet[i])
+        let newTweet = document.createElement('div');
+        newTweet.innerHTML=tweet[tweet.length-1];
+        console.log(newTweet)
+        console.log(tweetContainer)
+        tweetContainer.appendChild(newTweet)
+    
+    console.log(tweet)
 }
 
 
